5e42ed2f4b74c8629cfa6adebb6cbef8a3a3868e
root <root@kookaray.vuln.land>
2024-06-18 12:47:11 +0200

    update config

--- build-release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t hackinglab/alpine-tinyproxy:$1.0 -t hackinglab/alpine-tinyproxy:$1 -t hackinglab/alpine-tinyproxy:latest -f Dockerfile .
-
-docker push hackinglab/alpine-tinyproxy
-docker push hackinglab/alpine-tinyproxy:$1
-docker push hackinglab/alpine-tinyproxy:$1.0
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t hackinglab/alpine-tinyproxy:3.2.0 -t hackinglab/alpine-tinyproxy:3.2 -t hackinglab/alpine-tinyproxy:latest -f Dockerfile .
-
